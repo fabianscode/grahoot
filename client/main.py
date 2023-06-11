35a228b79733af1c57ddce170f44dc4a5a27640f
Fabian Specht <github@fabianspecht.xyz>
2023-06-12 01:01:25 +0200

    separation of functionality (socket)

--- client/main.py
+++ client/main.py
@@ -1,44 +1,66 @@
 import sys
 import getopt
-import json
-import ssl
+import tls_connection as conn
 
 def print_usage():
-    print("""main.py [-s server_address[:port]] [-g game_pin] [-d display_name]""")
+    print("""main.py [-n hostname] [-p port] [-g game_pin] [-d display_name] [-c certificate]""")
 
 
 def read_user_input(argv):
-    server_address = ""
+    hostname = ""
+    port = ""
     game_pin = ""
     display_name = ""
+    certificate_path = ""
 
-    opts, _ = getopt.getopt(argv, "hs:g:d:")
+    opts, _ = getopt.getopt(argv, "hn:p:g:d:c:")
 
     for opt, arg in opts:
         if opt == '-h':
             print_usage()
             sys.exit()
-        elif opt in ("-s"):
-            server_address = arg
+        elif opt in ("-n"):
+            hostname = arg
+        elif opt in ("-p"):
+            port = arg
         elif opt in ("-g"):
             game_pin = arg
         elif opt in ("-d"):
             display_name = arg
+        elif opt in ("-c"):
+            certificate_path = arg
 
-    if "" in [server_address, game_pin, display_name]:
+    if "" in [hostname, port, game_pin, display_name, certificate_path]:
         print_usage()
         sys.exit()
 
-    return server_address, game_pin, display_name
+    return hostname, int(port, base=10), game_pin, display_name, certificate_path
 
 
-def connect(server_address, game_pin, display_name):
-    print(f"Connecting to {server_address} ...")
+def connect(hostname, port, game_pin, display_name, certificate_path):
+    print(f"Connecting to {hostname}:{port} ...")
+
+    successfull = conn.open_connection(hostname, port, certificate_path)
+
+    if not successfull:
+        print("Connection failed!")
+        sys.exit()
+
+    print("Connection established!")
+
+    data = {
+        "username": display_name
+    }
+
+    conn.send_dict(data)
+
+    recv = conn.receive_dict()
+    print(recv["dieter"])
 
 
 def main(argv):
-    server_address, game_pin, display_name = read_user_input(argv)
-    connect(server_address, game_pin, display_name)
+    hostname, port, game_pin, display_name, certificate_path = read_user_input(argv)
+    connect(hostname, port, game_pin, display_name, certificate_path)
 
 
 if __name__ == "__main__":
--- server/server.py
+++ server/server.py
@@ -4,7 +4,22 @@ import json
 
 BUFF_SIZE = 1024
 
+def send_dict(dict_data, ssock):
+    dict_data = json.dumps(dict_data)
+    ssock.sendall(bytes(dict_data, encoding="utf-8"))
+
+def receive_dict(ssock):
+    data = ssock.recv(BUFF_SIZE)
+    data = data.decode("utf-8")
+    data = json.loads(data)
+
+    return data
+
+
 if __name__ == "__main__":
+    global established_connections
+    established_connections = []
+
     context = ssl.create_default_context(ssl.Purpose.CLIENT_AUTH)
     context.load_cert_chain(
             certfile="/etc/letsencrypt/live/grahoot.fabianspecht.xyz/fullchain.pem",
@@ -19,14 +34,21 @@ if __name__ == "__main__":
         while accepting:
             conn, (ip, port) = s.accept()
 
-            with context.wrap_socket(conn, server_side=True) as secure_conn:
+            with context.wrap_socket(conn, server_side=True) as ssock:
                 print(f"New connection from {ip}")
 
-                data = secure_conn.recv(BUFF_SIZE)
-                data = data.decode('utf-8')
-                data = json.loads(data)
+                established_connections.append(ssock)
+
+                data = receive_dict(ssock)
 
                 print(data["username"])
+
+                response = {
+                    "dieter": "hallo Mensch"
+                }
+
+                send_dict(response, ssock)
+
 
     except socket.timeout:
         pass
